8ba1afab16ea8ba7fbfe93eab9b79083f585e41d
mtrochimiuk <mtrochimiuk11@gmail.com>
2024-06-09 10:42:08 +0200

    api services fix

--- src/services/ShoppingListService.ts
+++ src/services/ShoppingListService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Category, Product } from "../types";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://localhost:3000/api',
   withCredentials: false,
   headers: {
     'Content-Type': 'application/json'
